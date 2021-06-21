2fbe4336cbd3c73e807fd00d6923e2a3e9cc7886
이길복 <pst1302@gmail.com>
2021-06-21 20:30:36 +0900

    2021월 06년 21일 <개발자 : 이길복> 테스트용 버튼 및 라벨 추가

--- Git_FirstProject/Git_FirstProject/Form1.Designer.cs
+++ Git_FirstProject/Git_FirstProject/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace Git_FirstProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(57, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(419, 179);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "테스트용 버튼";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(171, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "테스트용 라벨";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1036, 616);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
